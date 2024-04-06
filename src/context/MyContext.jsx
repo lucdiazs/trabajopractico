877b0e3e7b12883b7f00a60330af03f6139371fc
lucdiazs <salinasdiazluciano@gmail.com>
2024-04-06 20:36:34 -0300

    3ra subidda

--- src/context/MyContext.jsx
+++ src/context/MyContext.jsx
@@ -1,44 +1,34 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { createContext } from "react";
+import React, { useEffect, useState, createContext } from "react";
 
-export const MyContext = createContext()
+export const MyContext = createContext();
 
-const MyContextProvider = ({children}) => {
-    const [PizzasData, setPizzasData] = useState([])
-    const [carrito, setCarrito] = useState([])
-    
-    const DataUrl = '/pizzas.json'
+const MyContextProvider = ({ children }) => {
+    const [PizzasData, setPizzasData] = useState([]);
+    const [carrito, setCarrito] = useState([]);
 
-    useEffect(()=> {
+    const DataUrl = '/pizzas.json';
+
+    useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(DataUrl)
-                if(!response.ok){
-                    throw new Error('No se obtienen resultados de fetch')
+                const response = await fetch(DataUrl);
+                if (!response.ok) {
+                    throw new Error('No se obtienen resultados de fetch');
                 }
-                const dataFetch = await response.json()
-                setPizzasData(dataFetch)
-                
+                const dataFetch = await response.json();
+                setPizzasData(dataFetch);
             } catch (error) {
-                console.error('ha ocurrido un error',error)
+                console.error('ha ocurrido un error', error);
             }
-        }
-        fetchData()
-        
-        
-     },[]
-    
-     )
-     console.log(PizzasData)
+        };
+        fetchData();
+    }, []);
+
     return (
-        <>
-            <MyContext.Provider value={{PizzasData, setPizzasData,carrito, setCarrito}}>
-                {children}
-            </MyContext.Provider>
-        </>
-    )
+        <MyContext.Provider value={{ PizzasData, setPizzasData, carrito, setCarrito }}>
+            {children}
+        </MyContext.Provider>
+    );
+};
 
-
-}
-export default MyContextProvider
+export default MyContextProvider;
